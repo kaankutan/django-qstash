ff32f7fca48b87c9078743b8eb9ec37576956422
Justin Mitchel <jmitchel3@gmail.com>
2024-12-22 18:17:28 -0700

    Updated tests

--- tests/settings.py
+++ tests/settings.py
@@ -1,18 +1,31 @@
 from __future__ import annotations
 
-from typing import Any
-
 SECRET_KEY = "NOTASECRET"
 
-ALLOWED_HOSTS: list[str] = []
+DEBUG = True
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
+}
 
-DATABASES: dict[str, dict[str, Any]] = {}
-
-INSTALLED_APPS = [
-    "django_htmx",
+INSTALLED_APPS: list[str] = [
+    "django_qstash",
 ]
 
-MIDDLEWARE: list[str] = []
+MIDDLEWARE: list[str] = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+]
+
+ROOT_URLCONF = "tests.urls"
+QSTASH_TOKEN = "test-token"
+DJANGO_QSTASH_DOMAIN = "example.com"
+QSTASH_CURRENT_SIGNING_KEY = "current-key"
+QSTASH_NEXT_SIGNING_KEY = "next-key"
 
 TEMPLATES = [
     {
