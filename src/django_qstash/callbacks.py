3165e6f45c65033ef138c723149b8b5c54e7ff82
Justin Mitchel <jmitchel3@gmail.com>
2025-01-05 22:18:01 -0700

    Improved configuration error handling

--- src/django_qstash/callbacks.py
+++ src/django_qstash/callbacks.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from django.core.exceptions import ImproperlyConfigured
+
 from django_qstash.settings import DJANGO_QSTASH_DOMAIN
 from django_qstash.settings import DJANGO_QSTASH_WEBHOOK_PATH
 
@@ -8,6 +10,10 @@ def get_callback_url() -> str:
     """
     Get the callback URL based on the settings.
     """
+    if DJANGO_QSTASH_DOMAIN is None:
+        raise ImproperlyConfigured("DJANGO_QSTASH_DOMAIN is not set")
+    if DJANGO_QSTASH_WEBHOOK_PATH is None:
+        raise ImproperlyConfigured("DJANGO_QSTASH_WEBHOOK_PATH is not set")
     callback_domain = DJANGO_QSTASH_DOMAIN.rstrip("/")
     if not callback_domain.startswith(("http://", "https://")):
         callback_domain = f"https://{callback_domain}"
