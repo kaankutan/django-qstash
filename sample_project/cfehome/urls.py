d64254fb83460ccbaa78a36a89710798540d965b
Abdusshh <abdullah.enes189@gmail.com>
2025-01-19 23:15:32 +0300

    Add the notifications app URLs to cfehome project

--- sample_project/cfehome/urls.py
+++ sample_project/cfehome/urls.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from django_qstash.views import qstash_webhook_view
 
@@ -13,6 +13,7 @@ urlpatterns = [
     path("", views.index),
     path("admin/", admin.site.urls),
     path("qstash/webhook/", qstash_webhook_view),
+    path('', include('notifications.urls')),
 ]
 
 if settings.DEBUG:
