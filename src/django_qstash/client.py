8ae3a06c1710c96d64db1a5055e3d9c75cad251c
Justin Mitchel <jmitchel3@gmail.com>
2025-09-11 14:22:40 -0600

    Moved warning

--- src/django_qstash/client.py
+++ src/django_qstash/client.py
@@ -2,12 +2,14 @@ from __future__ import annotations
 
 import os
 import warnings
+from urllib.parse import urlparse
 
 from qstash import QStash
 
 from django_qstash.settings import QSTASH_TOKEN
 
 QSTASH_URL = os.environ.get("QSTASH_URL", None)
+UPSTASH_QSTASH_DOMAINS = ["upstash.io", "upstash.cloud", "upstash.com"]
 
 
 def init_qstash():
@@ -15,10 +17,12 @@ def init_qstash():
         "token": QSTASH_TOKEN,
     }
     if QSTASH_URL is not None:
-        warning_msg = f"\n\n\033[93mUsing {QSTASH_URL} as your QStash URL. \
-            \nThis configuration should only be used in development.\n\033[0m"
-        warnings.warn(warning_msg, RuntimeWarning, stacklevel=2)
-        kwargs["base_url"] = QSTASH_URL
+        domain = urlparse(QSTASH_URL).netloc
+        if domain.split(".")[-2:] not in UPSTASH_QSTASH_DOMAINS:
+            warning_msg = f"\n\n\033[93mUsing {QSTASH_URL} as your QStash URL. \
+                \nThis configuration should only be used in development.\n\033[0m"
+            warnings.warn(warning_msg, RuntimeWarning, stacklevel=2)
+            kwargs["base_url"] = QSTASH_URL
     return QStash(**kwargs)
 
 
